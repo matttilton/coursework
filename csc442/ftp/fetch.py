7aff3d0f6cffe82789efa3eb556a3b4f1066485e
Matthew Tilton <mtilton1996@gmail.com>
2017-04-10 21:46:52 -0500

    Fixed an issue. Added error checking so only the correct message is printed

--- csc442/ftp/fetch.py
+++ csc442/ftp/fetch.py
@@ -9,17 +9,25 @@ def convertBinaryToAscii():
     output = ''
 
     #decodes as 7 bit ascii
-    print('Decoding as 7 bit ascii')
     tmp = binaryPermissions[:(len(binaryPermissions) - (len(binaryPermissions) % 7))] #strips of trailing bits that dont fit into a multiple of 8
     values = [tmp[i:i+7] for i in range(0, len(tmp), 7)] #splits string into multiples of 7bits
     for each in values:
         integer = int(each, 2)#converts from binary into decimal
         if integer >= 32 or integer == 8 or integer == 13 or integer == 9 or integer == 10: #checks for printable characters
             output = output + (chr(integer))
-    print output
+
+    # error checking
+    flag = False
+    for each in output:
+        if ord(each) > 128 or ord(each) < 8 or 14 <= ord(each) <= 31: # Checking for invalid characters
+            flag = True
+        if (len(output) * 7) != len(tmp): # Checking for invalid lengths
+            flag = True
+
+    if (not flag):
+        print output
 
     #decodes as 8 bit ascii
-    print('Decoding as 8 bit ascii')
     output = '' #reset output
     tmp = binaryPermissions[:len(binaryPermissions) - (len(binaryPermissions) % 8)] #strips of trailing bits that dont fit into a multiple of 8
     values = [tmp[i:i+8] for i in range(0, len(tmp), 8)] #splits string into multiples of 8 bits
@@ -27,7 +35,17 @@ def convertBinaryToAscii():
         integer = int(each, 2) #converts from binary into decimal
         if integer >= 32 or integer == 8 or integer == 13 or integer == 9 or integer == 10: #checks for printable characters
             output = output + (chr(integer))
-    print output
+
+    # error checking
+    flag = False
+    for each in output:
+        if ord(each) > 128 or ord(each) < 8 or 14 <= ord(each) <= 31: #checking for invalid characters
+            flag = True
+        if (len(output) * 8) != len(tmp): #checking for invalid lengths
+            flag = True
+
+    if (not flag):
+        print output
 
 #converts the permissions to binary
 def convertPermissionsToBinary():
@@ -39,7 +57,6 @@ def convertPermissionsToBinary():
                 binaryPermissions = binaryPermissions + '1'
             else:
                 binaryPermissions = binaryPermissions + '0'
-    print 'All bits important'
     convertBinaryToAscii() #converts the binary to ascii
     binaryPermissions = ''
     for x in filePermissions:
@@ -52,7 +69,6 @@ def convertPermissionsToBinary():
                     binaryPermissions = binaryPermissions + '1'
                 else:
                     binaryPermissions = binaryPermissions + '0'
-    print 'Ignoring files with any of the first 3 bits set.'
     convertBinaryToAscii()
 
 #parses file permissions
@@ -65,7 +81,7 @@ def parseFilePermissions(x):
 #Establish FTP Connection using convenient library
 ftp = FTP('www.jeangourd.com')
 ftp.login()
-ftp.cwd('~/7')
+ftp.cwd('~/10')
 ftp.retrlines('LIST', parseFilePermissions) #get a list of the files 
 
 time.sleep(1) #wait some time may need to increase
