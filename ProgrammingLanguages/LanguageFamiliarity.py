f2ac2b6ed24201bb97e92fc15a977fe8275c8e3c
Matthew Tilton <mtilton@matthewtilton.com>
2017-12-04 07:59:45 -0600

    first assignment for programming languages

---
+++ ProgrammingLanguages/LanguageFamiliarity.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+# Instructions
+# Just run it
+
+proficient = ['Python', 'Javascript', 'HTML/CSS']
+familiar = ['C', 'C++', 'Java']
+marginal = ['Haskell']
+
+print('Proficient in: ')
+for a in proficient:
+    print('\t' + a)
+
+print('Familiar with: ')
+for b in familiar:
+    print('\t' + b)
+
+print('Marginal experience with: ')
+for c in marginal:
+    print('\t' + c)
